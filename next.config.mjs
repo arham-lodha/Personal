ccec93e4f58fef92da952b827b94bdcc9659007b
arham-lodha0317 <32276821+arham-lodha@users.noreply.github.com>
2025-12-19 08:40:40 -0600

    Github

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     reactStrictMode: true,
+    output: 'export',
+    images: {
+        unoptimized: true, // Required for static export
+    },
+    trailingSlash: true,
+    // If you are deploying to https://arham-lodha.github.io/Blog/, 
+    // uncomment the line below:
+    // basePath: '/Blog',
 };
 
 export default nextConfig;
